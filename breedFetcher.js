fbaa84dfcace916d758739dc0013a8802da466a8
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-11 21:06:05 +0000

    converting body to JSON object

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,11 @@
-const request = require('request')
+const request = require('request');
+
+request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, code, body) => {
+  console.log("error:", error);
+  console.log("code:", code && code.statusCode);
+  console.log("body", typeof body)
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
+
+})
