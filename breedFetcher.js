129f1f3b08e309b7d6948343eab307aa7964c3c4
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-11 21:47:38 +0000

    created mocha/chai testing for breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,15 @@
 const request = require('request');
-const breed = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, code, body) => {
-  // console.log("error:", error);
-  if (error) throw error;
-  // console.log("code:", code && code.statusCode);
-  // console.log("body", typeof body)
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  if (data[0] === undefined || code.statusCode !== 200) {
-    console.log(`Breed ${breed} was not found in thecatAPI`);
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breed, callback) => {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, code, body) => {
+    if (error) throw error;
+    const data = JSON.parse(body);
+    if (data[0] === undefined || code.statusCode !== 200) {
+      callback(`Breed ${breed} was not found in thecatAPI`, null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
