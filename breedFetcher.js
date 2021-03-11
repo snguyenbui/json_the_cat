865f0ab1f33db5de6e4fac0cf4650b22dd978274
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-03-11 21:21:49 +0000

    returns breed details and catches errors and breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,17 @@
 const request = require('request');
+const breed = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (error, code, body) => {
-  console.log("error:", error);
-  console.log("code:", code && code.statusCode);
-  console.log("body", typeof body)
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, code, body) => {
+  // console.log("error:", error);
+  if (error) throw error;
+  // console.log("code:", code && code.statusCode);
+  // console.log("body", typeof body)
   const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
-
-})
+  // console.log(data);
+  // console.log(typeof data);
+  if (data[0] === undefined || code.statusCode !== 200) {
+    console.log(`Breed ${breed} was not found in thecatAPI`);
+  } else {
+    console.log(data[0].description);
+  }
+});
